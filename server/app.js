04af56c98dfdae52e474e1f9341a1ac7796b40c3
Alan Wu <alanjialongwu@gmail.com>
2023-02-16 01:24:43 +1100

    added environment variables

--- server/app.js
+++ server/app.js
@@ -2,7 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import path from 'path';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const port = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
--- server/db/db.js
+++ server/db/db.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
-
-mongoose.connect("mongodb+srv://novelmtl:novelmtl1@cluster0.x3hiuox.mongodb.net/?retryWrites=true&w=majority", 
+import dotenv from 'dotenv';
+dotenv.config();
+mongoose.connect(process.env.URI, 
 {useNewUrlParser: true, useCreateIndex: true});
