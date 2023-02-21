93910c3eaa04efe1599ee6a335124ea3d91da013
Alan Wu <alanjialongwu@gmail.com>
2023-02-21 16:29:12 +1100

    basic url form submission

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {useFormik} from "formik";
+import axios from "axios";
+const App = () => {
+  const formik = useFormik({
+    initialValues: {
+      url: ""
+    },
+    onSubmit: values => {
+      console.log(values);
 
-function App() {
+      axios.post("localhost:5000/newChapter/", values)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+    }
+  });
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <form onSubmit={formik.handleSubmit}>
+    <label htmlFor="url">Enter URL</label>
+    <input
+      id="url"
+      name="url"
+      type="url"
+      onChange={formik.handleChange}
+      value={formik.values.email}
+    />
+    <button type="submit">Submit</button>
+  </form>
+  )
 }
 
 export default App;
